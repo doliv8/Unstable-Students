825d9e834cc66d7ae2f9bbf24f4c71561ce52a48
doliv Debian fisso <61095881+doliv8@users.noreply.github.com>
2025-02-13 16:30:49 +0100

    added dynamic titles for pick_aula_card menu; added src/debugging.c source file (not necessary)

--- src/debugging.h
+++ src/debugging.h
@@ -9,6 +9,7 @@
 
 #define DBG_INFO(fmt, ...) fprintf(stderr, "[INFO] [%s:%d] " fmt "\n", __FILE__, __LINE__, __VA_ARGS__);
 
+void dump_full_game(game_contextT *game_ctx);
+
 #endif // DEBUGGING_H
 #endif // DEBUG
-void dump_full_game(game_contextT *game_ctx);
