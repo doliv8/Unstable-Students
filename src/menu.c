ec7354536b02587079a06149053a7d47c47d7c39
doliv Debian fisso <61095881+doliv8@users.noreply.github.com>
2025-03-21 18:02:23 +0100

    fixed main menu statistics option crash (added main menu loop)

--- src/menu.c
+++ src/menu.c
@@ -10,37 +10,42 @@ game_contextT *main_menu(const char *provided_save) {
 	int option;
 	char *save_name;
 	game_contextT *game_ctx;
+	bool in_menu = true;
 
 	puts(MENU_ASCII_ART);
 
 	if (provided_save == NULL) {
-		do {
-			puts("[" TO_STRING(MENU_NEWGAME) "] Avvia una nuova partita");
-			puts("[" TO_STRING(MENU_LOADSAVE) "] Carica un salvataggio");
-			puts("[" TO_STRING(MENU_STATS) "] Consulta le statistiche");
-			puts("[" TO_STRING(MENU_QUIT) "] Esci dal gioco");
-			option = get_int();
-		} while (option < MENU_QUIT || option > MENU_LOADSAVE);
+		while (in_menu) {
+			do {
+				puts("[" TO_STRING(MENU_NEWGAME) "] Avvia una nuova partita");
+				puts("[" TO_STRING(MENU_LOADSAVE) "] Carica un salvataggio");
+				puts("[" TO_STRING(MENU_STATS) "] Consulta le statistiche");
+				puts("[" TO_STRING(MENU_QUIT) "] Esci dal gioco");
+				option = get_int();
+			} while (option < MENU_QUIT || option > MENU_STATS);
 
-		switch (option) {
-			case MENU_NEWGAME: {
-				display_full_stats();
-				game_ctx = new_game();
-				break;
-			}
-			case MENU_LOADSAVE: {
-				save_name = pick_save();
-				game_ctx = load_game(save_name);
-				free_wrap(save_name);
-				break;
-			}
-			case MENU_STATS: {
-				display_full_stats();
-				break;
-			}
-			case MENU_QUIT: {
-				exit(EXIT_SUCCESS);
-				break;
+			switch (option) {
+				case MENU_NEWGAME: {
+					display_full_stats();
+					game_ctx = new_game();
+					in_menu = false;
+					break;
+				}
+				case MENU_LOADSAVE: {
+					save_name = pick_save();
+					game_ctx = load_game(save_name);
+					free_wrap(save_name);
+					in_menu = false;
+					break;
+				}
+				case MENU_STATS: {
+					display_full_stats();
+					break;
+				}
+				case MENU_QUIT: {
+					exit(EXIT_SUCCESS);
+					break;
+				}
 			}
 		}
 	} else
